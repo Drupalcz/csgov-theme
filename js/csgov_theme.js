c661835659718089cde9461232bb4d0bb7f6e7ef
Petr Illek <petr@annertech.com>
2020-12-19 17:30:08 +0100

    Rename file. Remove artifacts.

--- js/csgov_theme.js
+++ js/csgov_theme.js
@@ -1,1 +1,37 @@
-!function(e){Drupal.behaviors.csgov_theme_BackToTop={attach:function(o,t){var i=e("body, html"),a=e(".back-to-top__link",o);e(window).scroll(function(){250<e(this).scrollTop()?a.addClass("is-visible"):a.removeClass("is-visible")}),a.click(function(o){i.animate({scrollTop:0},800,function(){i.attr("tabindex","-1").focus().removeAttr("tabindex")}),o.preventDefault()})}}}(jQuery);
+/**
+ * @file
+ * JS for Back To Top link.
+ */
+
+(function ($) {
+
+  Drupal.behaviors.csgov_theme_BackToTop = {
+    attach: function (context, settings) {
+      var $body = $('body, html');
+      var backToTop = $('.back-to-top__link', context);
+
+      // Toggle class on backToTop.
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 250) {
+          backToTop.addClass('is-visible');
+        } else {
+          backToTop.removeClass('is-visible');
+        }
+      });
+
+      // Scroll smoothly to top on click.
+      backToTop.click(function (event) {
+        $body.animate({
+          scrollTop: 0
+        }, 800, function () {
+          $body.attr('tabindex','-1').focus().removeAttr('tabindex');
+        });
+        event.preventDefault();
+      });
+
+    }
+  };
+
+})(jQuery);
+
+//# sourceMappingURL=czgov_theme.js.map
